a3197bb24b0e662953a9363acf315c4a581480c7
SriHarshaNagulakonda <sriharshanagulakonda@gmail.com>
2021-06-04 17:01:44 +0530

    create and update products

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,20 +1,4 @@
 export default {
-    primary: '#c2185b',
-    accent: '#ffc107',
-    lightcyan: '#E0FFFF', 
-    cyan: '#00FFFF',
-    silver: '#C0C0C0',
-    dimgray: '#696969',
-    moccasin: '#FFE4B5',
-    maroon: '#800000',
-    sandybrown: '#F4A460',
-    chocolate: '#D2691E',
-    lightyellow: '#FFFFE0',
-    lightskyblue: '#87CEFA',
-    deepskyblue: '#00BFFF',
-    lightblue: '#ADD8E6',
-    darkblue: '#00008B',
-    steelblue: '#4682B4',
-    lightgray: '#D3D3D3',
-    darkgray: '#A9A9A9',
-}
+    primary: '#C2185B',
+    accent: '#FFC107'
+};
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,46 +1,83 @@
-import React from "react";
-import {
-  FlatList,
-  TouchableOpacity,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { FlatList, Button, Platform } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-import ProductItem from "../../components/shop/ProductItem";
+import HeaderButton from '../../components/UI/HeaderButton';
+import ProductItem from '../../components/shop/ProductItem';
+import * as cartActions from '../../store/actions/cart';
+import Colors from '../../constants/Colors';
 
-const ProductsOverviewScreen = (props) => {
-  const products = useSelector((state) => state.products.availableProducts);
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
+
+  const selectItemHandler = (id, title) => {
+    props.navigation.navigate('ProductDetail', {
+      productId: id,
+      productTitle: title
+    });
+  };
+
   return (
     <FlatList
       data={products}
-      renderItem={(itemData) => (
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
         <ProductItem
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          id={itemData.item.id}
-          onViewDetail={() => {
-            props.navigation.navigate({
-              routeName: "ProductDetail",
-              params: {
-                productId: itemData.item.id,
-                productTitle: itemData.item.title,
-              },
-            });
+          onSelect={() => {
+            selectItemHandler(itemData.item.id, itemData.item.title);
           }}
-          onAddToCart={() => {}}
-        />
+        >
+          <Button
+            color={Colors.primary}
+            title="View Details"
+            onPress={() => {
+              selectItemHandler(itemData.item.id, itemData.item.title);
+            }}
+          />
+          <Button
+            color={Colors.primary}
+            title="To Cart"
+            onPress={() => {
+              dispatch(cartActions.addToCart(itemData.item));
+            }}
+          />
+        </ProductItem>
       )}
     />
   );
 };
 
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: "All Products",
+ProductsOverviewScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'All Products',
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+          onPress={() => {
+            navData.navigation.navigate('Cart');
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 export default ProductsOverviewScreen;
-
-const styles = StyleSheet.create({});
