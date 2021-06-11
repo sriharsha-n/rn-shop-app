1c8f83a0c68cc65a4691fbcb2f25b0ad44d7a1af
SriHarshaNagulakonda <sriharshanagulakonda@gmail.com>
2021-06-11 15:32:26 +0530

    user authenticatin

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import {
+  createSwitchNavigator,
+  createAppContainer,
+} from 'react-navigation';
 import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer,createSwitchNavigator } from "react-navigation";
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import { Platform } from 'react-native';
+import { Platform, SafeAreaView, Button, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-// import { createAnimatedSwitchNavigator } from 'react-navigation-animated-switch-navigator'
+import { useDispatch } from 'react-redux';
+import { DrawerItems } from 'react-navigation-drawer';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductScreen';
-import Colors from '../constants/Colors';
 import AuthScreen from '../screens/user/AuthScreen';
+import StartupScreen from '../screens/StartupScreen';
+import Colors from '../constants/Colors';
+import * as authActions from '../store/actions/auth';
 
 const defaultNavOptions = {
   headerStyle: {
@@ -66,23 +72,23 @@ const OrdersNavigator = createStackNavigator(
 );
 
 const AdminNavigator = createStackNavigator(
-    {
-      UserProducts: UserProductsScreen,
-      EditProduct: EditProductScreen
+  {
+    UserProducts: UserProductsScreen,
+    EditProduct: EditProductScreen
+  },
+  {
+    navigationOptions: {
+      drawerIcon: drawerConfig => (
+        <Ionicons
+          name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      )
     },
-    {
-      navigationOptions: {
-        drawerIcon: drawerConfig => (
-          <Ionicons
-            name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
-            size={23}
-            color={drawerConfig.tintColor}
-          />
-        )
-      },
-      defaultNavigationOptions: defaultNavOptions
-    }
-  );
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
 
 const ShopNavigator = createDrawerNavigator(
   {
@@ -93,15 +99,39 @@ const ShopNavigator = createDrawerNavigator(
   {
     contentOptions: {
       activeTintColor: Colors.primary
+    },
+    contentComponent: props => {
+      const dispatch = useDispatch();
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+            <DrawerItems {...props} />
+            <Button
+              title="Logout"
+              color={Colors.primary}
+              onPress={() => {
+                dispatch(authActions.logout());
+                // props.navigation.navigate('Auth');
+              }}
+            />
+          </SafeAreaView>
+        </View>
+      );
     }
   }
 );
 
-const AuthNavigator = createStackNavigator({
+const AuthNavigator = createStackNavigator(
+  {
     Auth: AuthScreen
-})
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
 
 const MainNavigator = createSwitchNavigator({
+  Startup: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopNavigator
 });
